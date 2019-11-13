fce0bbfc8a18737d856e7a0615d352c391fa1d68
Vnho <492148618@qq.com>
2019-11-13 14:51:50 +0800

    头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="login">
-   主页
-  </div>
+<!-- 布局 -->
+  <el-container>
+    <!-- 左右布局 -->
+    <el-aside>1</el-aside>
+    <!-- 大容器 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>2</el-header>
+      <!-- 主体 -->
+      <el-main>2
+        <!-- 二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
