0e95057089f2601add3cfe0fac21cee7a72de024
Vnho <492148618@qq.com>
2019-11-15 21:11:53 +0800

    文章列表的渲染完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -39,13 +39,38 @@
       </el-form>
     </el-card>
     <!-- 文章列表 -->
-    <el-card class="box-card">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="封面" width="180"></el-table-column>
-        <el-table-column prop="name" label="标题" width="180"></el-table-column>
-        <el-table-column prop="address" label="发布时间"></el-table-column>
-        <el-table-column prop="name" label="发布状态" width="180"></el-table-column>
-        <el-table-column prop="name" label="操作" width="180"></el-table-column>
+    <el-card class="box-card search">
+      <div slot="header" class="clearfix">
+        <span>共找到5680056条符合条件的内容</span>
+      </div>
+      <el-table :data="articals" style="width: 100%">
+        <el-table-column prop="date" label="封面" width="180">
+          <!--
+            1.自定义表格列获取图片地址
+            2.在template上声明 slot-scope="scpoe"就可以用scope.row获取遍历项
+            注意:只有当你需要在自定义表格列模板中访问遍历项的时候才这么做
+          -->
+          <template slot-scope="scope">
+            <!-- scope.row就相当于我们写的 v-for 里面的item -->
+            <!-- 取到数组对象中的图片相对应的地址(第一个)赋给图片地址 -->
+            <img :src="scope.row.cover.images[0]" width="50px" />
+          </template>
+        </el-table-column>
+        <el-table-column prop="title" label="标题" width="180"></el-table-column>
+        <el-table-column prop="status" label="发布状态" width="180">
+          <template slot-scope="scope">
+            <el-tag :type="articalStatus[scope.row.status].type" effect='dark'>
+              <span>{{articalStatus[scope.row.status].label}}</span>
+            </el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="pubdate" label="发布时间"></el-table-column>
+        <el-table-column prop="name" label="操作" width="180">
+          <template>
+            <el-button type="danger" size="mini">删除</el-button>
+            <el-button type="primary" size="mini">编辑</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
   </div>
@@ -63,48 +88,58 @@ export default {
         end_pubdate: ''
       },
       rangeDate: '',
-      tableData: [
+      articals: [],
+      articalStatus: [
         {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          type: '',
+          label: '草稿'
         },
         {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          type: 'warning',
+          label: '待审核'
         },
         {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          type: 'success',
+          label: '审核通过'
         },
         {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          type: 'danger',
+          label: '审核失败'
+        },
+        {
+          type: 'info',
+          label: '已删除'
         }
       ]
     }
   },
   created () {
-    // 在项目中,除了登录页'/login'不需要token,其他接口都需要token
-    const token = window.localStorage.getItem('user-token')
-    this.$axios({
-      method: 'GET',
-      url: '/articles',
-      // 这里是因为需要token数据验证要需要设置请求头，格式在文档中有显示
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(res => {
-        console.log(res)
+    this.loadArtical()
+  },
+  methods: {
+    loadArtical () {
+      // 在项目中,除了登录页'/login'不需要token,其他接口都需要token
+      const token = window.localStorage.getItem('user-token')
+      this.$axios({
+        method: 'GET',
+        url: '/articles',
+        // 这里是因为需要token数据验证要需要设置请求头，格式在文档中有显示
+        headers: { Authorization: `Bearer ${token}` }
       })
-      .catch(err => {
-        console.log(err)
-      })
+        .then(res => {
+          console.log(res)
+          this.articals = res.data.data.results
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
 
 <style>
+.search {
+  margin: 20px 0;
+}
 </style>
