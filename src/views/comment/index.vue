f144072abcee203b2b1a0c6cf183a6b77e32a2b1
Vnho <492148618@qq.com>
2019-11-19 01:32:19 +0800

    评论区域渲染完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,76 +5,78 @@
         <span>评论管理</span>
       </div>
       <!-- 表格 -->
-      <el-table
-      :data="this.articles"
-      style="width: 100%">
-      <el-table-column
-        prop="title"
-        label="标题"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="comment_status"
-        label="评论状态"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="total_comment-count"
-        label="总评论数据">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
+      <el-table :data="this.articles" style="width: 100%">
+        <el-table-column prop="title" label="标题" width="180"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数据"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column label="评论状态" width="180">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              @change="statusChange"
+            ></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" round size="mini">修改</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
+    <el-pagination
+    background
+    layout="prev, pager, next"
+    :total="totalPage"
+    @change="changePage"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'ArticleComment',
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      articles: []
+      articles: [],
+      totalPage: null
     }
   },
   created () {
     this.loadArticle()
   },
   methods: {
-    loadArticle () {
+    loadArticle (page) {
       this.$axios({
         method: 'GET',
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          page
         }
-      }).then(res => {
-        console.log(res)
-        this.articles = res.data.data.result
-      }).catch(err => {
-        console.log(err)
+      })
+        .then(res => {
+          console.log(res.data)
+          this.articles = res.data.data.results
+          this.totalPage = res.data.data.results.length
+        })
+        .catch(err => {
+          console.log(err, '获取数据失败')
+        })
+    },
+    changePage (page) {
+      this.loadArticle(page)
+      this.page = page
+    },
+    statusChange () {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: null
+        }
       })
     }
   }
