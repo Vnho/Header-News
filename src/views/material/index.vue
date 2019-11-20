a9fa08a0f9138ab680349dc8ace80a8304bc14f8
Vnho <492148618@qq.com>
2019-11-20 21:22:17 +0800

    素素材渲染和删除收藏功能标签渲染完成

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,23 +6,99 @@
         <el-button style="float: right; padding: 3px 0" type="text">上传图片</el-button>
       </div>
 
+      <!-- 选择框选择状态（全部或收藏） -->
+      <div>
+        <el-radio-group v-model="radio1">
+          <el-radio-button label="上海"></el-radio-button>
+          <el-radio-button label="北京"></el-radio-button>
+          <el-radio-button label="广州"></el-radio-button>
+          <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+      </div>
+
       <!--分栏偏移式 LayOut布局
          el-row 行
          el-col 列
       -->
       <el-row :gutter="20">
-        <el-col :xs="24" :sm="12" :md="6"><div class="grid-content bg-purple">123</div></el-col>
-        <el-col :xs="24" :sm="12" :md="6"><div class="grid-content bg-purple">456</div></el-col>
-        <el-col :xs="24" :sm="12" :md="6"><div class="grid-content bg-purple">789</div></el-col>
-        <el-col :xs="24" :sm="12" :md="6"><div class="grid-content bg-purple">999</div></el-col>
+        <el-col
+        :xs="24"
+        :sm="12"
+        :md="4"
+        v-for="item in images"
+        :key="item.id"
+        class="col-style"
+        >
+          <el-card
+          :body-style="{ padding: '0px' }"
+          style="text-align:center"
+          >
+            <img
+              :src="item.url"
+              class="image"
+              height='150'
+            />
+            <div
+            style="padding: 14px;" class="operation">
+            <!-- class 的对象语法 -->
+            <i :class="{
+              'el-icon-star-on':item.is_collected,
+              'el-icon-star-off':!item.is_collected
+            }"></i>
+            <!-- class 的普通判断语法 -->
+              <!-- <i :class="item.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"></i> -->
+              <i class="el-icon-delete-solid"></i>
+            </div>
+          </el-card>
+        </el-col>
       </el-row>
+      <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'MaterialConnect',
+  data () {
+    return {
+      images: []
+    }
+  },
+  created (page) {
+    this.loadMaterial()
+  },
+  methods: {
+    loadMaterial (page) {
+      this.$axios({
+        method: 'GET',
+        url: '/user/images',
+        params: {
+          collect: false,
+          page
+        }
+      }).then(res => {
+        console.log('获取素材成功', res)
+        this.images = res.data.data.results
+      }).catch(err => {
+        console.log('获取素材失败', err)
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style lang='less'>
+.col-style{
+  margin-bottom: 20px;
+  .operation{
+    display: flex;
+    justify-content: space-around;
+    font-size: 24px;
+  }
+}
 </style>
