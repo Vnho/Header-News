a9976c6b3f8a81be037513b9dce110dbc972cc8b
Vnho <492148618@qq.com>
2019-11-21 02:08:48 +0800

    账户管理-个人信息渲染完成

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,30 +6,35 @@
       </div>
 
       <!-- 用户信息表单 -->
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form" :model="accountData" label-width="80px">
         <el-form-item label="用户头像">
         <el-upload
             class="avatar-uploader"
             action="https://jsonplaceholder.typicode.com/posts/"
             :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            :before-upload="beforeAvatarUpload">
-            <img v-if="imageUrl" :src="imageUrl" class="avatar">
-            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            >
+            <img
+            width="80"
+            height="80"
+            style="border-radius:50%"
+            :src="accountData.photo">
+            <i class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
         </el-form-item>
         <el-form-item label="用户昵称">
-          <el-input v-model="form.name"></el-input>
+          <el-input v-model="accountData.name"></el-input>
         </el-form-item>
         <el-form-item label="个人介绍">
-          <el-input type="textarea" v-model="form.desc"></el-input>
+          <el-input type="textarea" v-model="accountData.intro"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号">
+          <el-input v-model="accountData.mobile"></el-input>
         </el-form-item>
         <el-form-item label="邮箱地址">
-          <el-input v-model="form.name"></el-input>
+          <el-input v-model="accountData.email"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-button type="primary">保存修改</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -41,16 +46,29 @@ export default {
   name: 'Account',
   data () {
     return {
-      form: {
+      accountData: {
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        email: '',
+        mobile: '',
+        intro: '',
+        photo: ''
       }
+    }
+  },
+  created () {
+    this.loadAccount()
+  },
+  methods: {
+    loadAccount () {
+      this.$axios({
+        methods: 'GET',
+        url: '/user/profile'
+      }).then(res => {
+        console.log('接收用户信息成功', res)
+        this.accountData = res.data.data
+      }).catch(err => {
+        console.log('接收用户信息失败', err)
+      })
     }
   }
 }
